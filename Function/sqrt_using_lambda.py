358337728877d148272dced60f7dbcb5b1c1016d
gurjardeepak602 <78991392+gurjardeepak602@users.noreply.github.com>
2022-05-30 11:22:56 +0530

    Update sqrt_using_lambda.py

--- Function/sqrt_using_lambda.py
+++ Function/sqrt_using_lambda.py
@@ -1,4 +1,4 @@
-#Write a Python program to square and cube every number in a given list of integers using Lambda.
+#Write a Python program to square every number in a given list of integers using Lambda.
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print("Original list of integers:")
 print(nums)
