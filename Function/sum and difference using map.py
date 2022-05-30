0fec00a616e22e83d594c5a4f38e0f915b68551f
gurjardeepak602 <78991392+gurjardeepak602@users.noreply.github.com>
2022-05-30 11:41:04 +0530

    Create sum and difference using map.py

---
+++ Function/sum and difference using map.py
@@ -0,0 +1,13 @@
+# Python program to add two given lists and find the difference between lists
+
+def addition_subtrction(x, y):
+    return x + y, x - y
+ 
+nums1 = [6, 5, 3, 9]
+nums2 = [0, 1, 7, 7]
+print("Original lists:")
+print(nums1)
+print(nums2)
+result = map(addition_subtrction, nums1, nums2)
+print("\nResult:")
+print(list(result))
