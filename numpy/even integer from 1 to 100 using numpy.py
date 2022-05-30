ffde5293dfc75a0e1598b389b0a79ec9c0f97db6
gurjardeepak602 <78991392+gurjardeepak602@users.noreply.github.com>
2022-05-30 11:44:20 +0530

    Create even integer from 1 to 100 using numpy.py

---
+++ numpy/even integer from 1 to 100 using numpy.py
@@ -0,0 +1,5 @@
+# NumPy program to create an array of all the even integers from 1 to 100.
+import numpy as np
+array=np.arange(2,101,2)
+print("Array of all the even integers from 1 to 100")
+print(array)
