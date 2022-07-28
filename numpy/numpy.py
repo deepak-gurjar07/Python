d4bfdda0810839770253c0c4e4e86571a925f64c
Deepak Gurjar <78991392+deepak-gurjar07@users.noreply.github.com>
2022-07-29 04:02:29 +0530

    Create numpy.py

---
+++ numpy/numpy.py
@@ -0,0 +1,11 @@
+
+# Python Program to create numpy array
+# filled with random values
+import numpy as np
+
+c = np.empty([3, 3])
+print("\nMatrix c : \n", c)
+
+d = np.empty([5, 3], dtype = int)
+print("\nMatrix d : \n", d)
+
